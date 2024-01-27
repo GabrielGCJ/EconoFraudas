192ea0c58a25e81451fea965e3c222baa8d6a27f
gabrielj <gabriel.ggcj@gmail.com>
2024-01-27 10:44:27 -0300

    feature/add-QuestionComponnt

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header/Header"
+import { QuestionComponnt } from "./components/QuestionComponent/QuestionComponent"
 import { GlobalStyle } from "./global"
 
 export const App = () => {
   return (
     <div>
       <Header/>
+      <QuestionComponnt/>
         {/* <h1>EconoFraudas</h1> */}
       <GlobalStyle/>
     </div>
